8e3d44eb6519ea9f4c6ae6687e0497e89c0a5b4f
Mohamed-Redouane <mohamedredhouanenemroud@gmail.com>
2025-01-19 22:04:31 -0500

    Automated Testing Pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', //app's development URL
+    baseUrl: 'http://localhost:5173', 
     viewportWidth: 1280,
     viewportHeight: 720,
     video: true,  
