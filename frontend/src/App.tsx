4fc0e1e84ce23a91d69bd65be472ada7a487078d
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-01-27 15:15:53 -0500

    [ADD] Added the Google Maps API to the front end
    Added a small template with the goolgle maps api and a key

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,32 @@
-
 import './App.css'
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+
+
 
 function App() {
  
 
   return (
-    <div className="p-4 bg-gradient-to-r from-blue-500 to-gray-700 text-white">
-    <h1>Tailwind CSS is working!</h1>
-  </div>
+    // Make the div have a fixed height and width
+    <div style={{height: '85vh', width: '75vw'}}>
+      <APIProvider apiKey={'AIzaSyAlwcko-mshwhZCheZpMjEJCwVBZ8Hhb-Q'} onLoad={() => console.log('Maps API has loaded.')}>
+
+      <Map
+      defaultZoom={10}
+      defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+      onCameraChanged={ (ev: MapCameraChangedEvent) =>
+        console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+      }>
+      </Map>
+
+
+
+
+      </APIProvider>
+      {/* <h1>Hello World</h1> */}
+    </div>
+  
+    
   );
 }
 
