004d64b8dab12903848ff4dabfee2d84c83a9dcf
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-01-28 12:26:50 -0500

    [FIXED] Made the code a bit cleaner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import MapComponent from './MapComponent';
 
 
 
@@ -7,26 +7,10 @@ function App() {
  
 
   return (
-    // Make the div have a fixed height and width
-    <div style={{height: '85vh', width: '75vw'}}>
-      <APIProvider apiKey={'AIzaSyAlwcko-mshwhZCheZpMjEJCwVBZ8Hhb-Q'} onLoad={() => console.log('Maps API has loaded.')}>
-
-      <Map
-      defaultZoom={10}
-      defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
-      onCameraChanged={ (ev: MapCameraChangedEvent) =>
-        console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-      }>
-      </Map>
-
-
-
-
-      </APIProvider>
-      {/* <h1>Hello World</h1> */}
-    </div>
-  
-    
+        // The div needs a constant height and width to display the map
+      <div style={{height: '85vh', width: '75vw'}}>
+        <MapComponent />
+      </div>
   );
 }
 
