881d422742a02df0c02b95f50ddb0cdb9b50f36e
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-01-30 15:53:15 -0500

    [ADD] Added testing for the Maps Component
    Ive added testing for the google maps api component

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,27 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173', 
+    baseUrl: "http://localhost:5173",
     viewportWidth: 1280,
     viewportHeight: 720,
-    video: true,  
-    screenshotsFolder: 'cypress/screenshots',
-    videosFolder: 'cypress/videos',                    
+    video: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     retries: {
-      runMode: 2,                     
-      openMode: 0                     
+      runMode: 2,
+      openMode: 0,
     },
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
+      require("@cypress/code-coverage/task")(on, config);
       return config;
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
