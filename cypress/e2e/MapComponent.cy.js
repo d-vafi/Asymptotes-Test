9a8fe10a558ceed3c2edf8ad18f0a6f482da4858
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-02-02 11:18:06 -0500

    [FIX] Fixed the test
    Now the remote test should pass

--- cypress/e2e/MapComponent.cy.js
+++ cypress/e2e/MapComponent.cy.js
@@ -1,7 +1,8 @@
 
 describe("MapComponent", () => {
     it("The Google Maps API is loaded", () => {
-        cy.visit("http://localhost:5174/")
+        // Changed the URL to the remote server
+        cy.visit("http://localhost:5173/")
         // The map component should exist
         cy.get('[data-testid="map"]').should("exist")
         // Check if the Google Maps API is loaded
